a51b109252c8077bcffc1c7e0e4ae831ebde6875
SHUBHAM <shubhamtiwary50@gmail.com>
2022-08-16 14:04:44 +0530

    Added problem statement

--- src/Customer.java
+++ src/Customer.java
@@ -1,9 +1,4 @@
-/*
-Create a class called Customer, which holds the data about customer, this will hold name, address, email, mobile and premium membership(yes/ no).
-This class must be properly encapsulated as well as will contain two constructor.
-This class must contain a method called displayCustomer(), which will display customer information.
-Write an implementation, create object1 by calling parameterized constructor, create second object by calling no- arguments constructor, display them and change there references means object1 should display object2 values and object2 will display object1 values.
- */
+
 public class Customer {
         String customerName;
         String address;
